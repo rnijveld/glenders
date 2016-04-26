51211afe326f1c5560d7464926c60e2636fece14
Ruben Nijveld <ruben@gewooniets.nl>
2016-04-26 12:29:00 +0200

    Updated config loading

--- src/config.rs
+++ src/config.rs
@@ -4,129 +4,229 @@ use std::fs::OpenOptions;
 use std::io::Read;
 use std::collections::BTreeMap;
 use std::error::Error;
-use std::i32;
-use std::u32;
-
-pub trait ConfigAt<'a, T> {
-    fn at(&'a self, pos: T) -> ConfigValue<'a>;
-}
+use std::ops::Index;
 
 pub trait ConfigGet<'a, T> {
     fn get(&'a self) -> Option<T>;
-}
 
-#[derive(Debug)]
-pub struct Config {
-    yaml: Yaml,
-}
+    fn unwrap_or(&'a self, default: T) -> T {
+        self.get().unwrap_or(default)
+    }
 
-#[derive(Debug)]
-enum IndexType<'a> {
-    String(&'a str),
-    Int(usize),
-}
+    fn unwrap(&'a self) -> T {
+        self.get().unwrap()
+    }
 
-#[derive(Debug)]
-pub struct ConfigValue<'a> {
-    index: IndexType<'a>,
-    parent: Option<&'a ConfigValue<'a>>,
-    root: &'a Config,
-}
+    fn unwrap_or_else<F: FnOnce() -> T>(&'a self, f: F) -> T {
+        self.get().unwrap_or_else(f)
+    }
 
-impl<'a> ConfigValue<'a> {
-    fn get_yaml_value(&self) -> &Yaml {
-        let mut v = Vec::new();
-        let mut current = self;
-        loop {
-            v.push(&current.index);
+    fn is_some(&'a self) -> bool {
+        self.get().is_some()
+    }
 
-            match current.parent {
-                Some(p) => current = p,
-                None => break,
-            }
-        }
-
-        let mut yval = &self.root.yaml;
-        for item in v.into_iter().rev() {
-            match *item {
-                IndexType::String(s) => yval = &yval[s],
-                IndexType::Int(i) => yval = &yval[i],
-            }
-        }
-
-        return yval;
+    fn is_none(&'a self) -> bool {
+        self.get().is_none()
     }
 }
 
-impl<'a> ConfigGet<'a, i64> for ConfigValue<'a> {
+#[derive(Clone, PartialEq, PartialOrd, Debug, Eq, Ord, Hash)]
+pub enum Config {
+    Real(String),
+    Int(i64),
+    String(String),
+    Bool(bool),
+    Array(Vec<Config>),
+    Hash(BTreeMap<Config, Config>),
+    Null,
+    Invalid
+}
+
+impl Config {
+    pub fn is_invalid(&self) -> bool {
+        match *self {
+            Config::Invalid => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_null(&self) -> bool {
+        match *self {
+            Config::Null => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_hash(&self) -> bool {
+        match *self {
+            Config::Hash(_) => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_array(&self) -> bool {
+        match *self {
+            Config::Array(_) => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_int(&self) -> bool {
+        match *self {
+            Config::Int(_) => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_bool(&self) -> bool {
+        match *self {
+            Config::Bool(_) => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_string(&self) -> bool {
+        match *self {
+            Config::String(_) => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_real(&self) -> bool {
+        match *self {
+            Config::Real(_) => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_numeric(&self) -> bool {
+        self.is_real() || self.is_int()
+    }
+}
+
+impl<'a> ConfigGet<'a, i64> for Config {
     fn get(&'a self) -> Option<i64> {
-        match *self.get_yaml_value() {
-            Yaml::Integer(i) => Some(i),
+        match *self {
+            Config::Int(i) => Some(i),
             _ => None,
         }
     }
 }
 
-impl<'a> ConfigGet<'a, u64> for ConfigValue<'a> {
+impl<'a> ConfigGet<'a, u64> for Config {
     fn get(&'a self) -> Option<u64> {
-        match *self.get_yaml_value() {
-            Yaml::Integer(i) if i >= 0 => Some(i as u64),
+        match *self {
+            Config::Int(i) if i >= 0 => Some(i as u64),
             _ => None,
         }
     }
 }
 
-impl<'a> ConfigGet<'a, isize> for ConfigValue<'a> {
+impl<'a> ConfigGet<'a, isize> for Config {
     fn get(&'a self) -> Option<isize> {
-        match *self.get_yaml_value() {
-            Yaml::Integer(i) if i >= isize::min_value() as i64 &&
-                                i <= isize::max_value() as i64 => Some(i as isize),
+        match *self {
+            Config::Int(i) if i >= isize::min_value() as i64 &&
+                              i <= isize::max_value() as i64 => Some(i as isize),
             _ => None,
         }
     }
 }
 
-impl<'a> ConfigGet<'a, usize> for ConfigValue<'a> {
+impl<'a> ConfigGet<'a, usize> for Config {
     fn get(&'a self) -> Option<usize> {
-        match *self.get_yaml_value() {
-            Yaml::Integer(i) if i >= usize::min_value() as i64 &&
-                                i <= usize::max_value() as i64 => Some(i as usize),
+        match *self {
+            Config::Int(i) if i >= 0 &&
+                              i <= usize::max_value() as i64 => Some(i as usize),
             _ => None,
         }
     }
 }
 
-impl<'a> ConfigGet<'a, u32> for ConfigValue<'a> {
+impl<'a> ConfigGet<'a, u32> for Config {
     fn get(&'a self) -> Option<u32> {
-        match *self.get_yaml_value() {
-            Yaml::Integer(i) if i >= u32::min_value() as i64 && i <= u32::max_value() as i64 => {
-                Some(i as u32)
-            }
+        match *self {
+            Config::Int(i) if i >= 0 &&
+                              i <= u32::max_value() as i64 => Some(i as u32),
             _ => None,
         }
     }
 }
 
-impl<'a> ConfigGet<'a, i32> for ConfigValue<'a> {
+impl<'a> ConfigGet<'a, bool> for Config {
+    fn get(&'a self) -> Option<bool> {
+        match *self {
+            Config::Bool(b) => Some(b),
+            _ => None,
+        }
+    }
+}
+
+impl<'a> ConfigGet<'a, f32> for Config {
+    fn get(&'a self) -> Option<f32> {
+        match *self {
+            Config::Real(ref r) => {
+                match r.parse() {
+                    Ok(f) => Some(f),
+                    _ => None,
+                }
+            },
+            Config::Int(i) => Some(i as f32),
+            _ => None,
+        }
+    }
+}
+
+impl<'a> ConfigGet<'a, f64> for Config {
+    fn get(&'a self) -> Option<f64> {
+        match *self {
+            Config::Real(ref r) => {
+                match r.parse() {
+                    Ok(f) => Some(f),
+                    _ => None,
+                }
+            },
+            Config::Int(i) => Some(i as f64),
+            _ => None,
+        }
+    }
+}
+
+impl<'a> ConfigGet<'a, i32> for Config {
     fn get(&'a self) -> Option<i32> {
-        match *self.get_yaml_value() {
-            Yaml::Integer(i) if i >= i32::min_value() as i64 && i <= i32::max_value() as i64 => {
-                Some(i as i32)
-            }
+        match *self {
+            Config::Int(i) if i >= i32::min_value() as i64 &&
+                              i <= i32::max_value() as i64 => Some(i as i32),
             _ => None,
         }
     }
 }
 
-impl<'a, T> ConfigGet<'a, Vec<T>> for ConfigValue<'a>
-    where for<'b> ConfigValue<'b>: ConfigGet<'b, T>
+impl<'a> ConfigGet<'a, String> for Config {
+    fn get(&'a self) -> Option<String> {
+        match *self {
+            Config::String(ref s) => Some(s.clone()),
+            _ => None,
+        }
+    }
+}
+
+impl<'a> ConfigGet<'a, &'a str> for Config {
+    fn get(&'a self) -> Option<&'a str> {
+        match *self {
+            Config::String(ref s) => Some(&s[..]),
+            _ => None,
+        }
+    }
+}
+
+impl<'a, T> ConfigGet<'a, Vec<T>> for Config where Config: ConfigGet<'a, T>
 {
     fn get(&'a self) -> Option<Vec<T>> {
-        match *self.get_yaml_value() {
-            Yaml::Array(ref a) => {
+        match *self {
+            Config::Array(ref a) => {
                 let mut v: Vec<T> = Vec::new();
                 for i in 0..a.len() {
-                    let maybe_val = self.at(i).get();
+                    let maybe_val = self[i].get();
 
                     match maybe_val {
                         Some(val) => v.push(val),
@@ -140,14 +240,14 @@ impl<'a, T> ConfigGet<'a, Vec<T>> for ConfigValue<'a>
     }
 }
 
-impl<'a, T, U> ConfigGet<'a, (T, U)> for ConfigValue<'a>
-    where for<'b> ConfigValue<'b>: ConfigGet<'b, T> + ConfigGet<'b, U>
+impl<'a, T, U> ConfigGet<'a, (T, U)> for Config
+    where for<'b> Config: ConfigGet<'b, T> + ConfigGet<'b, U>
 {
-    fn get(&'a self) -> Option<(T, U)> {
-        match *self.get_yaml_value() {
-            Yaml::Array(ref a) if a.len() == 2 => {
-                let x = self.at(0).get();
-                let y = self.at(1).get();
+    fn get(&self) -> Option<(T, U)> {
+        match *self {
+            Config::Array(ref a) if a.len() == 2 => {
+                let x = self[0].get();
+                let y = self[1].get();
                 if x.is_none() || y.is_none() {
                     None
                 } else {
@@ -159,15 +259,15 @@ impl<'a, T, U> ConfigGet<'a, (T, U)> for ConfigValue<'a>
     }
 }
 
-impl<'a, T, U, V> ConfigGet<'a, (T, U, V)> for ConfigValue<'a>
-    where for<'b> ConfigValue<'b>: ConfigGet<'b, T> + ConfigGet<'b, U> + ConfigGet<'b, V>
+impl<'a, T, U, V> ConfigGet<'a, (T, U, V)> for Config
+    where for<'b> Config: ConfigGet<'b, T> + ConfigGet<'b, U> + ConfigGet<'b, V>
 {
-    fn get(&'a self) -> Option<(T, U, V)> {
-        match *self.get_yaml_value() {
-            Yaml::Array(ref a) if a.len() == 3 => {
-                let x = self.at(0).get();
-                let y = self.at(1).get();
-                let z = self.at(2).get();
+    fn get(&self) -> Option<(T, U, V)> {
+        match *self {
+            Config::Array(ref a) if a.len() == 3 => {
+                let x = self[0].get();
+                let y = self[1].get();
+                let z = self[2].get();
                 if x.is_none() || y.is_none() || z.is_none() {
                     None
                 } else {
@@ -179,20 +279,16 @@ impl<'a, T, U, V> ConfigGet<'a, (T, U, V)> for ConfigValue<'a>
     }
 }
 
-impl<'a, S, T, U, V> ConfigGet<'a, (S, T, U, V)> for ConfigValue<'a>
-    where for<'b> ConfigValue<'b>:
-          ConfigGet<'b, S>
-        + ConfigGet<'b, T>
-        + ConfigGet<'b, U>
-        + ConfigGet<'b, V>
+impl<'a, S, T, U, V> ConfigGet<'a, (S, T, U, V)> for Config
+    where for<'b> Config: ConfigGet<'b, S> + ConfigGet<'b, T> + ConfigGet<'b, U> + ConfigGet<'b, V>
 {
     fn get(&'a self) -> Option<(S, T, U, V)> {
-        match *self.get_yaml_value() {
-            Yaml::Array(ref a) if a.len() == 4 => {
-                let w = self.at(0).get();
-                let x = self.at(1).get();
-                let y = self.at(2).get();
-                let z = self.at(3).get();
+        match *self {
+            Config::Array(ref a) if a.len() == 4 => {
+                let w = self[0].get();
+                let x = self[1].get();
+                let y = self[2].get();
+                let z = self[3].get();
 
                 if w.is_none() || x.is_none() || y.is_none() || z.is_none() {
                     None
@@ -205,56 +301,135 @@ impl<'a, S, T, U, V> ConfigGet<'a, (S, T, U, V)> for ConfigValue<'a>
     }
 }
 
-impl<'a> ConfigAt<'a, &'a str> for ConfigValue<'a> {
-    fn at(&'a self, pos: &'a str) -> ConfigValue<'a> {
-        ConfigValue {
-            parent: Some(&self),
-            root: self.root,
-            index: IndexType::String(pos),
+impl<'a, T> ConfigGet<'a, [T; 1]> for Config where Config: ConfigGet<'a, T> {
+    fn get(&'a self) -> Option<[T; 1]> {
+        match *self {
+            Config::Array(ref a) if a.len() == 1 => {
+                let x = self[0].get();
+                if x.is_none() {
+                    None
+                } else {
+                    Some([x.unwrap()])
+                }
+            }
+            _ => None,
         }
     }
 }
 
-impl<'a> ConfigAt<'a, &'a str> for Config {
-    fn at(&'a self, pos: &'a str) -> ConfigValue<'a> {
-        ConfigValue {
-            parent: None,
-            root: &self,
-            index: IndexType::String(pos),
+impl<'a, T> ConfigGet<'a, [T; 2]> for Config where Config: ConfigGet<'a, T> {
+    fn get(&'a self) -> Option<[T; 2]> {
+        match *self {
+            Config::Array(ref a) if a.len() == 2 => {
+                let x = self[0].get();
+                let y = self[1].get();
+
+                if x.is_none() || y.is_none() {
+                    None
+                } else {
+                    Some([x.unwrap(), y.unwrap()])
+                }
+            }
+            _ => None,
         }
     }
 }
 
-impl<'a> ConfigAt<'a, usize> for ConfigValue<'a> {
-    fn at(&'a self, pos: usize) -> ConfigValue<'a> {
-        ConfigValue {
-            parent: Some(&self),
-            root: self.root,
-            index: IndexType::Int(pos),
+impl<'a, T> ConfigGet<'a, [T; 3]> for Config where Config: ConfigGet<'a, T> {
+    fn get(&'a self) -> Option<[T; 3]> {
+        match *self {
+            Config::Array(ref a) if a.len() == 3 => {
+                let x = self[0].get();
+                let y = self[1].get();
+                let z = self[2].get();
+
+                if x.is_none() || y.is_none() || z.is_none() {
+                    None
+                } else {
+                    Some([x.unwrap(), y.unwrap(), z.unwrap()])
+                }
+            }
+            _ => None,
         }
     }
 }
 
-impl<'a> ConfigAt<'a, usize> for Config {
-    fn at(&'a self, pos: usize) -> ConfigValue<'a> {
-        ConfigValue {
-            parent: None,
-            root: &self,
-            index: IndexType::Int(pos),
+impl<'a, T> ConfigGet<'a, [T; 4]> for Config where Config: ConfigGet<'a, T> {
+    fn get(&'a self) -> Option<[T; 4]> {
+        match *self {
+            Config::Array(ref a) if a.len() == 4 => {
+                let w = self[0].get();
+                let x = self[1].get();
+                let y = self[2].get();
+                let z = self[3].get();
+
+                if w.is_none() || x.is_none() || y.is_none() || z.is_none() {
+                    None
+                } else {
+                    Some([w.unwrap(), x.unwrap(), y.unwrap(), z.unwrap()])
+                }
+            }
+            _ => None,
+        }
+    }
+}
+
+static INVALID_VALUE: Config = Config::Invalid;
+impl <'a> Index<&'a str> for Config {
+    type Output = Config;
+
+    fn index(&self, idx: &'a str) -> &Config {
+        let key = Config::String(idx.to_owned());
+        match *self {
+            Config::Hash(ref h) => h.get(&key).unwrap_or(&INVALID_VALUE),
+            _ => &INVALID_VALUE,
+        }
+    }
+}
+
+impl Index<usize> for Config {
+    type Output = Config;
+
+    fn index(&self, idx: usize) -> &Config {
+        match *self {
+            Config::Array(ref v) => v.get(idx).unwrap_or(&INVALID_VALUE),
+            _ => &INVALID_VALUE,
         }
     }
 }
 
 impl Config {
     pub fn from_yaml(yaml: Yaml) -> Config {
-        Config { yaml: yaml }
+        match yaml {
+            Yaml::Real(r) => Config::Real(r),
+            Yaml::Integer(i) => Config::Int(i),
+            Yaml::String(s) => Config::String(s),
+            Yaml::Boolean(b) => Config::Bool(b),
+            Yaml::Null => Config::Null,
+            Yaml::BadValue => Config::Invalid,
+            Yaml::Array(orig) => {
+                let mut res = Vec::new();
+                for val in orig {
+                    res.push(Config::from_yaml(val));
+                }
+                Config::Array(res)
+            }
+            Yaml::Hash(orig) => {
+                let mut res = BTreeMap::new();
+                for (key, value) in orig {
+                    res.insert(Config::from_yaml(key), Config::from_yaml(value));
+                }
+                Config::Hash(res)
+            },
+            Yaml::Alias(_) => panic!("Aliases in yaml file not supported"),
+        }
     }
 
     pub fn new() -> Config {
-        Config::from_yaml(Yaml::Hash(BTreeMap::new()))
+        Config::Null
     }
 
-    pub fn from_file_or_empty(filename: &str) -> io::Result<Config> {
+    pub fn from_yaml_file_or_empty(filename: &str) -> io::Result<Config> {
         match OpenOptions::new().read(true).open(filename) {
             // found file, get contents
             Ok(mut file) => {
--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,19 @@ mod config;
 
 use glium::DisplayBuild;
 use std::error::Error;
-use config::*;
+pub use config::*;
 
 fn main() {
-    let config = Config::from_file_or_empty("./config.yml").unwrap_or_else(|e| {
+    let config = Config::from_yaml_file_or_empty("./config.yml").unwrap_or_else(|e| {
         println!("Could not read config: {}", e.description());
         std::process::exit(5);
     });
 
-    let (width, height) = config.at("graphics").at("dimensions").get().unwrap_or((800, 600));
-
+    let (width, height) = config["graphics"]["dimensions"].unwrap_or((800, 600));
+    let strs: Vec<&str> = config["strs"].unwrap_or(Vec::new());
+    println!("{:?}", strs);
+    // let fullscreen = config["graphics"]["fullscreen"].unwrap_or(false);
+    // let dims: [f64; 4] = config["dims"].unwrap_or_else(|| [0.0; 4]);
 
     let builder = glium::glutin::WindowBuilder::new().with_dimensions(width, height);
 
